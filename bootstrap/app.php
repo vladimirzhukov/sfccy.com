ac97b1a1ac313a7ce79b9669e862ac5a01904ef6
vladimirzhukov <zhukov.vladimir@gmail.com>
2025-08-12 10:51:52 +0300

    Update CSRF token validation to exclude specific authentication routes

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -16,6 +16,9 @@ return Application::configure(basePath: dirname(__DIR__))
             \Mcamara\LaravelLocalization\Middleware\LaravelLocalizationRedirectFilter::class,
             \Mcamara\LaravelLocalization\Middleware\LaravelLocalizationViewPath::class
         ]);
+        $middleware->validateCsrfTokens(except: [
+            '/auth/signup', '/auth/signin', '/auth/forgot',
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
